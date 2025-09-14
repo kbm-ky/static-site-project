efa5ab89a0a39df9ae43f5b9941ff397457c65cb
Kyle Martin <kbm-ky@outlook.com>
2025-09-14 13:19:26 -0500

    Split text nodes

--- src/functions.py
+++ src/functions.py
@@ -24,3 +24,31 @@ def text_node_to_html_node(text_node: TextNode) -> HtmlNode:
 
         case _:
             raise Exception(f'unsupported type: {text_node.text_type.value}')
+
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+
+        text = node.text
+        if text == '':
+            raise Exception('how did a text node w/o text get through?')
+
+        while len(text) > 0:
+            t = text.split(delimiter, 1)
+            if t[0] != '':
+                new_node = TextNode(t[0], TextType.TEXT)
+                new_nodes.append(new_node)
+            if len(t) == 1:
+                break
+            text = t[1]
+            t = text.split(delimiter, 1)
+            if len(t) == 1:
+                raise Exception(f'unbalanced "{delimiter}": {node.text}')
+            new_node = TextNode(t[0], text_type)
+            new_nodes.append(new_node)
+            text = t[1]
+
+    return new_nodes
--- src/test_functions.py
+++ src/test_functions.py
@@ -2,9 +2,9 @@ import unittest
 
 from htmlnode import HtmlNode, LeafNode, ParentNode
 from textnode import TextNode, TextType
-from functions import text_node_to_html_node
+from functions import text_node_to_html_node, split_nodes_delimiter
 
-class TestFunctions(unittest.TestCase):
+class TestTextToHtml(unittest.TestCase):
     def test_text(self):
         node = TextNode("This is a text node", TextType.TEXT)
         html_node = text_node_to_html_node(node)
@@ -40,6 +40,77 @@ class TestFunctions(unittest.TestCase):
         expect = f'<img src="/path/to/image.png" alt="This is an image node">'
         actual = text_node_to_html_node(node).to_html()
         self.assertEqual(expect, actual)
+
+
+class TestNodeSplitter(unittest.TestCase):
+    def test_split_code(self):
+        node = TextNode('This is text with a `code block` word', TextType.TEXT)
+        expect = [
+            TextNode('This is text with a ', TextType.TEXT),
+            TextNode('code block', TextType.CODE),
+            TextNode(' word', TextType.TEXT),
+        ]
+        actual = split_nodes_delimiter([node], '`', TextType.CODE)
+        self.assertEqual(expect, actual)
+        
+    def test_split_no_delim(self):
+        node = TextNode('This text has no delimiters', TextType.TEXT)
+        expect =[
+            TextNode('This text has no delimiters', TextType.TEXT)
+        ]
+        actual = split_nodes_delimiter([node], '**', TextType.BOLD)
+        self.assertEqual(expect, actual)
+
+
+    def test_split_all_italic(self):
+        node = TextNode('_I am all italics_', TextType.TEXT)
+        expect = [
+            TextNode('I am all italics', TextType.ITALIC),
+        ]
+        actual = split_nodes_delimiter([node], '_', TextType.ITALIC)
+        self.assertEqual(expect, actual)
+
+
+    def test_split_at_end(self):
+        node = TextNode('Only **this is bold**', TextType.TEXT)
+        expect = [
+            TextNode('Only ', TextType.TEXT),
+            TextNode('this is bold', TextType.BOLD),
+        ]
+        actual = split_nodes_delimiter([node], '**', TextType.BOLD)
+        self.assertEqual(expect, actual)
+
+    def test_should_error(self):
+        node = TextNode('I have _unbalanced delims', TextType.TEXT)
+        with self.assertRaisesRegex(Exception, 'unbalanced "_"'):
+            split_nodes_delimiter([node], '_', TextType.ITALIC)
+
+
+        node = TextNode('_More _imbalance_', TextType.TEXT)
+        with self.assertRaisesRegex(Exception, 'unbalanced "_"'):
+            split_nodes_delimiter([node], '_', TextType.ITALIC)
+
+
+    def test_mutiple_nodes(self):
+        nodes = [
+            TextNode('A lot of **bold** items in **this** case', TextType.TEXT),
+            TextNode('**so much** is **bold**!', TextType.TEXT),
+        ]
+        expect = [
+            TextNode('A lot of ', TextType.TEXT),
+            TextNode('bold', TextType.BOLD),
+            TextNode(' items in ', TextType.TEXT),
+            TextNode('this', TextType.BOLD),
+            TextNode(' case', TextType.TEXT),
+            TextNode('so much', TextType.BOLD),
+            TextNode(' is ', TextType.TEXT),
+            TextNode('bold', TextType.BOLD),
+            TextNode('!', TextType.TEXT),
+        ]
+        actual = split_nodes_delimiter(nodes, '**', TextType.BOLD)
+        self.assertEqual(expect, actual)
+
+
 
 if __name__ == "__main__":
     unittest.main()
