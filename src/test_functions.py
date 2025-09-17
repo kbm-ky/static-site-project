b7392261a9c49a1d77eac343bdf0ffd212bcdbea
Kyle Martin <kbm-ky@outlook.com>
2025-09-17 17:29:23 -0500

    Text to text nodes plumbed up

--- src/test_functions.py
+++ src/test_functions.py
@@ -9,6 +9,7 @@ from functions import (
     extract_markdown_links,
     split_nodes_image,
     split_nodes_link,
+    text_to_textnodes,
 )
                     
 
@@ -203,6 +204,25 @@ class TestSplitLinks(unittest.TestCase):
             ],
             new_nodes,
         )
+
+class TestTextToNodes(unittest.TestCase):
+    def test_text_to_textnodes(self):
+        text = 'This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev), tada!'
+        want = [            
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+            TextNode(", tada!", TextType.TEXT),
+        ]
+        got = text_to_textnodes(text)
+        self.assertListEqual(want, got)
 
 
 if __name__ == "__main__":
